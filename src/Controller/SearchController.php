cec4421fcc09c0d78c06202b6a4526f631cb3f7f
Eleni Xynou <Eleni-Margarita.Xynou@etu.univ-paris1.fr>
2025-03-25 14:48:06 +0100

    reservation ok dans bdd

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,11 +1,15 @@
 <?php
+// filepath: src/Controller/SearchController.php
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Repository\AnnonceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class SearchController extends AbstractController
 {
@@ -24,12 +28,38 @@ class SearchController extends AbstractController
     }
 
     #[Route('/reservation/{id}', name: 'app_reservation')]
-    public function reservation(int $id, Request $request, AnnonceRepository $annonceRepository): Response
+    public function reservation(int $id, Request $request, AnnonceRepository $annonceRepository, EntityManagerInterface $entityManager): Response
     {
         $annonce = $annonceRepository->find($id);
 
         if ($request->isMethod('POST')) {
-            // Gérer la réservation ici
+            // Check if the user is logged in
+            $user = $this->getUser();
+            if (!$user) {
+                // If the user is not logged in, redirect to the login page
+                throw new AccessDeniedException('Vous devez être connecté pour effectuer une réservation.');
+                // Or, alternatively, redirect to the login page:
+                // return $this->redirectToRoute('app_login');
+            }
+
+            // Get the dates from the form
+            $dateDebut = new \DateTime($request->request->get('date_debut'));
+            $dateFin = new \DateTime($request->request->get('date_fin'));
+
+            // Create a new Reservation object
+            $reservation = new Reservation();
+            $reservation->setDateDebut($dateDebut);
+            $reservation->setDateFin($dateFin);
+            $reservation->setIdAnnonce($annonce);
+            $reservation->setIdUtilisateur($user);
+
+            // Persist the reservation to the database
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre réservation a été effectuée avec succès !');
+
+            return $this->redirectToRoute('app_home'); // Redirect to a confirmation page or home
         }
 
         return $this->render('reservation/form.html.twig', [
