bbbb0082f80401b73e259935a6266fd01de156ee
Manal Oumlil <manaloumlil01@gmail.com>
2025-04-03 12:40:19 +0200

    accountcontroller corrected

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -1,22 +1,19 @@
 <?php
 
-
-// src/Controller/CompteController.php
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class AccountController extends AbstractController
 {
     #[Route('/compte', name: 'app_compte')]
     #[IsGranted('ROLE_USER')]
-    public function index(UserInterface $user): Response // !user connecté ici
+    public function index(Utilisateur $user): Response
     {
-        
         $annonces = $user->getAnnonces();
         $reservations = $user->getReservations();
 
