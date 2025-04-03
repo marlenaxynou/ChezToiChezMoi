e24c1d84b2393cd00d304fc2211785b83b52e5b5
Eleni Xynou <Eleni-Margarita.Xynou@etu.univ-paris1.fr>
2025-04-03 22:21:22 +0200

    edit info on profile

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use App\Form\EditProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class AccountController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte', name: 'app_compte')]
     #[IsGranted('ROLE_USER')]
     public function index(): Response
@@ -24,6 +35,29 @@ class AccountController extends AbstractController
             'user' => $user,
             'annonces' => $annonces,
             'reservations' => $reservations,
+        ]);
+    }
+
+    #[Route('/compte/edit', name: 'app_compte_edit')]
+    #[IsGranted('ROLE_USER')]
+    public function edit(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour.');
+
+            return $this->redirectToRoute('app_compte');
+        }
+
+        return $this->render('compte/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
